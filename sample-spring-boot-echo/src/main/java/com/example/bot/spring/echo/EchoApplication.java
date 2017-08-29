f5d2d38664fde7200a8a1e17b3e8edef0da5a7e6
infowarnet <31415452+infowarnet@users.noreply.github.com>
2017-08-29 10:35:29 +0700

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -27,11 +27,8 @@ import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
+import java.io.*;
+import java.net.*;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -47,7 +44,7 @@ public class EchoApplication {
 
     
         
-         return new TextMessage(callURL("http://cdn.crunchify.com/wp-content/uploads/code/json.sample.txt"));
+         return new TextMessage(getUrlSource("http://cdn.crunchify.com/wp-content/uploads/code/json.sample.txt"));
         //URL website = new URL("http://cdn.crunchify.com/wp-content/uploads/code/json.sample.txt");
         
 //        return new TextMessage(response.toString());
@@ -58,34 +55,18 @@ public class EchoApplication {
         System.out.println("event: " + event);
     }
    
- public static String callURL(String myURL) {
-		System.out.println("Requeted URL:" + myURL);
-		StringBuilder sb = new StringBuilder();
-		URLConnection urlConn = null;
-		InputStreamReader in = null;
-		try {
-			URL url = new URL(myURL);
-			urlConn = url.openConnection();
-			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
-		in.close();
-		} catch (Exception e) {
-			throw new RuntimeException("Exception while calling URL:"+ myURL, e);
-		} 
- 
-		return sb.toString();
-	}
+  private static String getUrlSource(String url) throws IOException {
+            URL yahoo = new URL(url);
+            URLConnection yc = yahoo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    yc.getInputStream(), "UTF-8"));
+            String inputLine;
+            StringBuilder a = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                a.append(inputLine);
+            in.close();
+
+            return a.toString();
+        }
   
 }
