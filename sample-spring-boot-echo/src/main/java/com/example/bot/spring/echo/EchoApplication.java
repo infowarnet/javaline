ad1e6786f7dc6e964c372c66243b92e54004422c
infowarnet <31415452+infowarnet@users.noreply.github.com>
2017-08-29 10:46:03 +0700

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -27,13 +27,35 @@ import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 
-import java.io.*;
-import java.net.*;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
+ public static void Connect() throws Exception{
+ 
+  //Set URL
+  URL url = new URL("http://website here.com");
+  URLConnection spoof = url.openConnection();
+ 
+  //Spoof the connection so we look like a web browser
+  spoof.setRequestProperty( "User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)" );
+  BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
+  String strLine = "";
+ 
+  //Loop through every line in the source
+  while ((strLine = in.readLine()) != null){
+ 
+   //Prints each line to the console
+   System.out.println(strLine);
+  }
+ 
+  System.out.println("End of page.");
+ }
+    
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
@@ -42,10 +64,15 @@ public class EchoApplication {
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event)  {
         System.out.println("event: " + event);
         //return new TextMessage("TEST: "+event.getMessage().getText());
-
+  try{
+   //Calling the Connect method
+   Connect();
+  }catch(Exception e){
+ 
+  }
     
         
-         return new TextMessage(getUrlSource("http://cdn.crunchify.com/wp-content/uploads/code/json.sample.txt"));
+         return new TextMessage("TEST");
         //URL website = new URL("http://cdn.crunchify.com/wp-content/uploads/code/json.sample.txt");
         
 //        return new TextMessage(response.toString());
@@ -56,18 +83,6 @@ public class EchoApplication {
         System.out.println("event: " + event);
     }
    
-  private static String getUrlSource(String url) throws IOException {
-            URL yahoo = new URL(url);
-            URLConnection yc = yahoo.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    yc.getInputStream(), "UTF-8"));
-            String inputLine;
-            StringBuilder a = new StringBuilder();
-            while ((inputLine = in.readLine()) != null)
-                a.append(inputLine);
-            in.close();
-
-            return a.toString();
-        }
+  
   
 }
